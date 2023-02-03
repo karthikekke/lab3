fb64cb90b986c6f373c6ceed8ea1f93989b46697
Karthik Ekke <ekke@auto.ece.pdx.edu>
2023-02-02 23:46:52 -0800

    lab3

---
+++ syn/scripts/adder_1a_tb.sv
@@ -0,0 +1,23 @@
+
+
+
+
+
+module adder_1a_tb;
+reg [3:0] a;
+reg [3:0] b;
+reg cin=0;
+wire [3:0] sum;
+wire cout;
+adder_1a rca(a,b,cin,sum,cout);
+initial begin
+for(int i=0;i<16;i=i+1) begin
+#2; a=i;
+	for(int j=0;j<16;j=j+1) begin
+		#2; b=j;
+		
+	end
+end
+end
+endmodule
+
